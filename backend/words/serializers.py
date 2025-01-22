8105406280fb8e8c351c83f6cb471e057ccd947f
keanu-a <keanucaloua@gmail.com>
2025-01-21 19:45:33 -0800

    Fixed serializers and created word view

--- backend/words/serializers.py
+++ backend/words/serializers.py
@@ -1,48 +1,7 @@
 from rest_framework import serializers
-from words.models import Word, English, Translation, Aspect, LinkedWord
+from .models import Word, English, Translation, Aspect, LinkedWord
 
 
-class WordSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Word
-        fields = [
-            'pid',
-            'tagalog',
-            'root',
-            'accents',
-            'alternate_spelling',
-            'is_irregular_verb',
-            'note',
-            'audio_url'
-        ]
-        
-    def validate(self, data):
-        # Validate if word is a verb
-        if any(translation.part_of_speech == Translation.PartOfSpeech.VERB for translation in data['translations']):
-            if "is_irregular_verb" not in data or data["is_irregular_verb"] == None:
-                raise serializers.ValidationError("Must provide 'is_irregular_verb' if word is a verb")
-            
-            aspects = data.get("aspects", [])
-            if data["aspects"].length != 3:
-                raise serializers.ValidationError("Must provide 3 aspects if word is a verb")
-            
-            aspect_types = {aspect.aspect for aspect in aspects}
-            required_aspects = {Aspect.AspectType.COMPLETED, Aspect.AspectType.UNCOMPLETED, Aspect.AspectType.CONTEMPLATED}
-            
-            if aspect_types != required_aspects:
-                raise serializers.ValidationError(
-                    f"Word must have one of each aspect: {required_aspects}. No more, no less, and no duplicates."
-                )
-                
-        # Validate accents
-        accents = data.get("accents", [])
-        if not isinstance(accents, list) or not all(isinstance(i, int) for i in accents):
-            raise serializers.ValidationError("Accents for words must be an array of integers")
-                
-        return data
-        
-        
 class EnglishSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -62,6 +21,7 @@ class TranslationSerializer(serializers.ModelSerializer):
         
         
 class AspectSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Aspect
         fields = [
@@ -89,3 +49,51 @@ class LinkedWordSerializer(serializers.ModelSerializer):
             'english_meaning',
             'audio_url'
         ]
+
+
+class WordSerializer(serializers.ModelSerializer):
+    
+    translations = TranslationSerializer(many=True)
+    aspects = AspectSerializer(many=True)
+    linked_words = LinkedWordSerializer(many=True)
+
+    class Meta:
+        model = Word
+        fields = [
+            'pid',
+            'tagalog',
+            'root',
+            'accents',
+            'alternate_spelling',
+            'is_irregular_verb',
+            'note',
+            'audio_url',
+            'translations',
+            'aspects',
+            'linked_words'
+        ]
+        
+    def validate(self, data):
+        # Validate if word is a verb
+        if any(translation.part_of_speech == Translation.PartOfSpeech.VERB for translation in data['translations']):
+            if "is_irregular_verb" not in data or data["is_irregular_verb"] == None:
+                raise serializers.ValidationError("Must provide 'is_irregular_verb' if word is a verb")
+            
+            aspects = data.get("aspects", [])
+            if data["aspects"].length != 3:
+                raise serializers.ValidationError("Must provide 3 aspects if word is a verb")
+            
+            aspect_types = {aspect.aspect for aspect in aspects}
+            required_aspects = {Aspect.AspectType.COMPLETED, Aspect.AspectType.UNCOMPLETED, Aspect.AspectType.CONTEMPLATED}
+            
+            if aspect_types != required_aspects:
+                raise serializers.ValidationError(
+                    f"Word must have one of each aspect: {required_aspects}. No more, no less, and no duplicates."
+                )
+                
+        # Validate accents
+        accents = data.get("accents", [])
+        if not isinstance(accents, list) or not all(isinstance(i, int) for i in accents):
+            raise serializers.ValidationError("Accents for words must be an array of integers")
+                
+        return data
